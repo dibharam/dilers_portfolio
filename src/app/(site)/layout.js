6590fd2ee84f7096ef83c06948b7b0473354a7df
dibharam <di.bharam@gmail.com>
2025-08-21 22:59:23 +0100

    Added train rail on front site and fixed alinging issues

--- src/app/(site)/layout.js
+++ src/app/(site)/layout.js
@@ -1,13 +1,19 @@
+
 import SiteShell from "@/components/layout/SiteShell";
-import ParallaxScene from "@/components/rails/ParallaxScene";
+import RailScene from "@/components/rails/RailScene"; // (or keep ParallaxScene if you didn’t rename)
 import TrainNav from "@/components/rails/TrainNav";
 
 export default function SiteLayout({ children }) {
   return (
     <div className="relative min-h-screen">
-      <ParallaxScene />
-      <TrainNav />
-      <SiteShell>{children}</SiteShell>
+      <RailScene />
+      <SiteShell>
+        {/* Train sits UNDER the header, ABOVE the content */}
+        <div className="mx-auto max-w-5xl px-4">
+          <TrainNav />
+        </div>
+        {children}
+      </SiteShell>
     </div>
   );
 }
